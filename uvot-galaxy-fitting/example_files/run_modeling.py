c9f630d028885765446b79c3cacd78502f0736f7
Lea Hagen <lhagen@stsci.edu>
2018-03-09 14:19:37 -0500

    clarifying example in run_modeling.py

--- uvot-galaxy-fitting/example_files/run_modeling.py
+++ uvot-galaxy-fitting/example_files/run_modeling.py
@@ -21,7 +21,7 @@ def run_modeling():
 
     # the list of filters to put in the grid
     # -> these will be used as the labels
-    filter_list = ['fuv','nuv','w2','m2','w1','u','g','r','i','3.6']
+    filter_list_grid = ['fuv','nuv','w2','m2','w1','u','g','r','i','3.6']
 
     # the full path to the corresponding filter transmission curves
     temp = ['galex_fuv.txt','galex_nuv.txt', 'swift_w2.txt','swift_m2.txt','swift_w1.txt',
@@ -34,13 +34,13 @@ def run_modeling():
 
     
     # create grids
-    create_grids.create_grids(filter_list, filter_transmission_curves, metallicity)
+    create_grids.create_grids(filter_list_grid, filter_transmission_curves, metallicity)
 
     
     # ---- modeling-related things
 
     # filters to model
-    filter_list = ['fuv','nuv','w2','m2','w1','g','r','i','3.6']
+    filter_list_data = ['fuv','nuv','w2','m2','w1','g','r','i','3.6']
 
     # ... do your procedure to read in the data ...
 
@@ -49,15 +49,15 @@ def run_modeling():
     phot_err = np.array([0.05, 0.05, 0.05, 0.05, 0.05, 0.05, 0.05, 0.05, 0.15])
     
     # number of galaxies
-    n_region = len(phot)
+    n_gal = 1
 
     # distance
     dist = 9.0 # Mpc
 
     # run modeling
-    for i in range(n_region):
-        mags = {f:phot[i] for i,f in enumerate(filter_list)}
-        mag_errs = {f:phot_err[i] for i,f in enumerate(filter_list)}
+    for g in range(n_gal):
+        mags = {f:phot[i] for i,f in enumerate(filter_list_data)}
+        mag_errs = {f:phot_err[i] for i,f in enumerate(filter_list_data)}
         run_chi2.run_chi2(mags, mag_errs, metallicity, dist, 'UGC00695')
 
     
